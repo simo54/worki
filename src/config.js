f31c00285db0339562ebfffe1145ef30f73447c9
simone <sorzesimone1@gmail.com>
2020-11-10 09:46:52 +0100

    changing settings for deployment

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // This file is mainly used for deployment, if you want to try this locally you may want to comment out line n.3 and uncomment line n.4
 
-const apiUrl = "https://workiserver.netlify.app/";
+const apiUrl = "https://workiserver.herokuapp.com/";
 // const apiUrl = "http://localhost:5000/";
 
 export default apiUrl;
