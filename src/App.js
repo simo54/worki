3b7aead8d3b3fad7fb86a5da3f3bd7cabd61a5b1
simone <sorzesimone1@gmail.com>
2020-11-13 09:45:28 +0100

    uploading gif for overview section

--- src/App.js
+++ src/App.js
@@ -14,41 +14,39 @@ import IsAuthenticated from "./Authentication/IsAuthenticated";
 export default function App(props) {
   return (
     <Router>
-      <div>
-        <Navigation />
-        <Switch>
-          {/* <Route
-            path="/jobs"
-            component={(props) => (
-              <IsAuthenticated
-                WrappedComponent={JobList}
-                {...props}
-                typeOfUser={"user"}
-              />
-            )}
-          /> */}
-          <Route path="/jobs" component={JobList} />
-          <Route path="/howitworks" component={Howitworks} />
-          <Route path="/usersignup" component={UserSignUp} />
-          <Route path="/employer/profile">
+      <Navigation />
+      <Switch>
+        <Route
+          path="/jobs"
+          component={(props) => (
             <IsAuthenticated
-              WrappedComponent={EmployerProfile}
-              typeOfUser={"employer"}
+              WrappedComponent={JobList}
               {...props}
-            />
-          </Route>
-          <Route path="/user/profile">
-            <IsAuthenticated
-              WrappedComponent={UserProfile}
               typeOfUser={"user"}
-              {...props}
             />
-          </Route>
-          <Route path="/employersignup" component={EmployerSignup} />
-          <Route path="/" exact component={Homepage} />
-        </Switch>
-        <Footer />
-      </div>
+          )}
+        />
+        {/* <Route path="/jobs" component={JobList} /> */}
+        <Route path="/howitworks" component={Howitworks} />
+        <Route path="/usersignup" component={UserSignUp} />
+        <Route path="/employer/profile">
+          <IsAuthenticated
+            WrappedComponent={EmployerProfile}
+            typeOfUser={"employer"}
+            {...props}
+          />
+        </Route>
+        <Route path="/user/profile">
+          <IsAuthenticated
+            WrappedComponent={UserProfile}
+            typeOfUser={"user"}
+            {...props}
+          />
+        </Route>
+        <Route path="/employersignup" component={EmployerSignup} />
+        <Route path="/" exact component={Homepage} />
+      </Switch>
+      <Footer />
     </Router>
   );
 }
